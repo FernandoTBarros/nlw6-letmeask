c48a9e669ab53a304d9c33401b7b1436cec39cbb
Fernando T. Barros <fernando@deltasi.com.br>
2021-06-26 02:52:28 -0300

    Finishing class 4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,7 +36,12 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert('Room does not exists')
+      alert('Room does not exists');
+    }
+
+    if (roomRef.val().endedAt) {
+      alert('Room already ended');
+      return;
     }
 
     history.push(`rooms/${roomCode}`)
